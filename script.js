6c1ef8b55c8b9716dd28dfc81d558220b0426e91
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-11-28 21:24:14 -0300

    implementação de animação scroll

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#titulo', { 
+    rotate: {x: 1000, y: 20, z: 0}, duration: 2000 
+})
+
+sr.reveal('#autor', { duration: 2000 });
+
+sr.reveal('.paragraph', { duration: 2000 });
